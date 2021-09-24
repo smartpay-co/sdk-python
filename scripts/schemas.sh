ced5f1b7756db1a3fa9ef79aff19a6bb05f46713
othree <othree@smartpay.co>
2021-09-24 16:17:47 +0800

    Improve schema.sh

--- scripts/schemas.sh
+++ scripts/schemas.sh
@@ -3,7 +3,10 @@
 FILES=smartpay/schemas/*.json
 
 for f in $FILES; do
+  echo $f
   OUT=smartpay/schemas/$(basename -s .jtd.json $f | sed -e 's/[.-]/_/g').py
-  printf '%s\n\n%s%s\n' "import jtd" "$(basename -s .jtd.json $f | sed -e 's/[.-]/_/g')_schema = jtd.Schema.from_dict(" "$(cat $f | sed -e 's/ true/ True/')" ")" > $OUT
+  printf '%s\n\n%s%s\n' "import jtd" "$(basename -s .jtd.json $f | sed -e 's/[.-]/_/g')_schema = jtd.Schema.from_dict(" "$(cat $f | sed -e 's/ true/ True/' | sed -e 's/ false/ False/')" ")" > $OUT
   autopep8 --in-place $OUT
 done
+
+echo "done"
